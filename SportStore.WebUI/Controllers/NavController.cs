4cab7a164142db303da90c036f542a1a152e9a8f
huinalam <huinalam@gmail.com>
2015-02-06 02:24:58 +0900

    [Add] 8-8

--- SportStore.WebUI/Controllers/NavController.cs
+++ SportStore.WebUI/Controllers/NavController.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SportStore.Domain.Abstract;
 
 namespace SportStore.WebUI.Controllers
 {
     public class NavController : Controller
     {
-        // GET: Nav
-        public string Menu()
+        private IProductRepository repository;
+
+        public NavController(IProductRepository repo)
         {
-            return "Heelo from NavControl";
+            repository = repo;
+        }
+
+        // GET: Nav
+        public PartialViewResult Menu()
+        {
+            IEnumerable<string> categories = repository.Products
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x);
+            return PartialView(categories);
         }
     }
 }
