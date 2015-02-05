8595e45e8967c673edca1f0ffe222312a88b670b
huinalam <huinalam@gmail.com>
2015-02-06 02:15:03 +0900

    [Add] 8-6

---
+++ SportStore.WebUI/Controllers/NavController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportStore.WebUI.Controllers
+{
+    public class NavController : Controller
+    {
+        // GET: Nav
+        public string Menu()
+        {
+            return "Heelo from NavControl";
+        }
+    }
+}
