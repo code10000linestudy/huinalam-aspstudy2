543a1c44309e8228f8b05782aa20a99a0a982910
huinalam <huinalam@gmail.com>
2015-02-07 17:16:56 +0900

    [Add] 9-2

---
+++ SportStore.WebUI/Infrastructure/Binders/CartModelBinder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Web;
+using System.Web.Mvc;
+using SportStore.Domain.Entities;
+
+namespace SportStore.WebUI.Infrastructure.Binders
+{
+    public class CartModelBinder : IModelBinder
+    {
+        private const string sessionKey = "Cart";
+
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            Cart cart = null;
+            if (controllerContext.HttpContext.Session != null)
+            {
+                cart = (Cart) controllerContext.HttpContext.Session[sessionKey];
+            }
+
+            if (cart == null)
+            {
+                cart = new Cart();
+                if (controllerContext.HttpContext.Session != null)
+                {
+                    controllerContext.HttpContext.Session[sessionKey] = cart;
+                }
+            }
+            return cart;
+        }
+    }
+}
